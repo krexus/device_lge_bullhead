aa13e63db39d5c076433ff2b23b3c6dcd4f9524f
argraur <argraur@gmail.com>
2018-08-12 21:26:01 +0000

    device: hals: add gatekeeper hal service
    Signed-off-by: argraur <argraur@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- device.mk
+++ device.mk
@@ -192,7 +192,8 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-service \
     android.hardware.keymaster@3.0-service.rc \
     android.hardware.nfc@1.1-service \
-    android.hardware.nfc@1.1-service.rc
+    android.hardware.nfc@1.1-service.rc \
+    android.hardware.gatekeeper@1.0-service
 
 PRODUCT_PACKAGES += \
     gralloc.msm8992 \
